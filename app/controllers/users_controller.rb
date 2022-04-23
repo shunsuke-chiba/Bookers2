74384eb59e310c4481c549feb0733aca5fdf767b
shunsuke-chiba <hoshigame.297@gmail.com>
2022-04-23 13:18:17 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,20 @@ class UsersController < ApplicationController
 
   def edit
     @user =User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to users_path(@user.id)
+    end
   end
   
   def update
     @user =User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
   end
   
   
